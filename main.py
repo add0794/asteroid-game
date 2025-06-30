47a736eea9a00886da0dbe47a4b2c600cd011084
add0794 <add0794@gmail.com>
2025-06-30 10:43:35 -0400

    add new player

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame
 from constants import *
-
+import player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    clock = pygame.time.Clock()
-    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    new_player = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         clock.tick(60) / 1000
         screen.fill(color='black')
