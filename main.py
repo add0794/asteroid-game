4c945c9a42100f4a1b0eb40c90139a80c238349f
add0794 <add0794@gmail.com>
2025-06-30 09:34:10 -0400

    Add clock

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    tick = 60
+
     while True:
+        clock.tick(tick)
         screen.fill(color='black')
         pygame.display.flip()
         
